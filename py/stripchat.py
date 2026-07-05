91b3c1dc0f043e07f408cde9842b30fb31298131
FGBLH <pengli08211@gmail.com>
2026-07-05 18:33:27 +0800

    Update stripchat.py

--- py/stripchat.py
+++ py/stripchat.py
@@ -20,7 +20,10 @@ class Spider(Spider):
 
     def init(self, extend="{}"):
         origin = 'https://zh.stripchat.com'
+        self.host = origin
         self.headers = {
+            'Origin': origin,
+            'Referer': f"{origin}/",
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:144.0) Gecko/20100101 Firefox/144.0'
         }
         self.stripchat_key = self.decode_key_compact()

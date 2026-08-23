d61d33e1b69c36dbde35c26ac119ec32ebd0cc98
FGBLH <pengli08211@gmail.com>
2026-08-23 22:10:34 +0800

    Update stripchat.py

--- py/stripchat.py
+++ py/stripchat.py
@@ -392,7 +392,7 @@ class Spider(Spider):
       # --- 线路3: StripOl ---
       elif id.startswith('sacf'):
         rsp = self.session_get(
-            f'https://edge-hls.sacfedge.com/hls/{sid}/master/{sid}_auto.m3u8?playlistType=lowLatency'
+            f'https://edge-hls.growcdnssedge.com/hls/{sid}/master/{sid}_auto.m3u8?playlistType=lowLatency'
         ).text
         lines = rsp.strip().split('\n')
         psch, pkey = 'v2', self.stripchat_pkey
